f66a2329c0befb9b0faca2fb724c0c490d45ce8e
Kusum Rathi <kusumrathi8401@gmail.com>
2026-09-22 12:17:28 +0530

    Create operators.py

---
+++ 01_variables_datatypes/02_operators/operators.py
@@ -0,0 +1,57 @@
+🐍 Day 2 — Operators:
+
+# 1. Arithmetic operators
+a = 10
+b = 3
+
+print(a + b)   # Addition
+print(a - b)   # Subtraction
+print(a * b)   # Multiplication
+print(a / b)   # Division
+print(a // b)  # Floor division
+print(a % b)   # Modulus
+print(a ** b)  # Exponent
+
+
+# 2. Comparison operators
+a = 10
+b = 5
+
+print(a > b)
+print(a < b)
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a <= b)
+
+
+# 3. Assignment operators
+x = 10
+
+x += 5
+print(x)
+
+x -= 3
+print(x)
+
+x *= 2
+print(x)
+
+x /= 4
+print(x)
+
+
+# 4. Logical operators
+age = 23
+
+print(age > 18 and age < 30)
+print(age < 18 or age > 20)
+print(not(age > 18))
+
+
+# 5. Membership operators
+name = "Kusum"
+
+print("K" in name)
+print("z" in name)
+print("x" not in name)
